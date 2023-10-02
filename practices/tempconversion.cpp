4ec7dfba85c18e0dbbcfcd2b73fbda9e50e484a9
newalvaro9 <75276952+newalvaro9@users.noreply.github.com>
2023-10-02 17:04:34 +0200

    some bug fixes

--- practices/tempconversion.cpp
+++ practices/tempconversion.cpp
@@ -6,19 +6,26 @@ int main()
     char from_unit;
     char to_unit;
 
-    std::cout << "F = Fahrenheit\nC = Celsius\nK = Kelvin\nFrom which unit would you like to convert? ";
+    std::cout << "----- Temperature conversor -----" << std::endl;
+
+
+    std::cout << "F = Fahrenheit\nC = Celsius\nK = Kelvin" << std::endl;
+
+    std::cout << "From which unit would you like to convert? ";
     std::cin >> from_unit;
 
-    if (from_unit != 'C' || 'c' || 'F' || 'f' || 'K' || 'k')
+    if (from_unit != 'C' && from_unit != 'c' && from_unit != 'F' && from_unit != 'f' && from_unit != 'K' && from_unit != 'k')
     {
         std::cout << "Invalid temperature unit!";
         return 1;
     }
 
-    std::cout << "F = Fahrenheit\nC = Celsius\nK = Kelvin\nTo which unit would you like to convert? ";
+    std::cout << "F = Fahrenheit\nC = Celsius\nK = Kelvin" << std::endl;
+
+    std::cout << "To which unit would you like to convert? ";
     std::cin >> to_unit;
 
-    if (to_unit != 'C' || 'c' || 'F' || 'f' || 'K' || 'k')
+    if (from_unit != 'C' && from_unit != 'c' && from_unit != 'F' && from_unit != 'f' && from_unit != 'K' && from_unit != 'k')
     {
         std::cout << "Invalid temperature unit!";
         return 1;
@@ -32,7 +39,7 @@ int main()
             std::cin >> temperature;
 
             double result = temperature * 9 / 5 + 32;
-            std::cout << temperature << "°C is " << result << "°F";
+            std::cout << temperature << "°C are " << result << "°F";
         }
         else if (to_unit == 'K' || to_unit == 'k')
         {
@@ -40,7 +47,7 @@ int main()
             std::cin >> temperature;
 
             double result = temperature + 273.15;
-            std::cout << temperature << "°C is " << result << "K";
+            std::cout << temperature << "°C are " << result << "K";
         }
     }
     else if (from_unit == 'F' || from_unit == 'f')
@@ -51,7 +58,7 @@ int main()
             std::cin >> temperature;
 
             double result = (temperature - 32) * 5 / 9;
-            std::cout << temperature << "°F is " << result << "°C";
+            std::cout << temperature << "°F are " << result << "°C";
         }
         else if (to_unit == 'K' || to_unit == 'k')
         {
@@ -59,7 +66,7 @@ int main()
             std::cin >> temperature;
 
             double result = (temperature - 32) * 5 / 9 + 273.15;
-            std::cout << temperature << "°F is " << result << "K";
+            std::cout << temperature << "°F are " << result << "K";
         }
     }
     else if (from_unit == 'K' || from_unit == 'k')
@@ -70,7 +77,7 @@ int main()
             std::cin >> temperature;
 
             double result = temperature - 273.15;
-            std::cout << temperature << "K is " << result << "°C";
+            std::cout << temperature << "K are " << result << "°C";
         }
         else if (to_unit == 'F' || to_unit == 'f')
         {
@@ -78,7 +85,7 @@ int main()
             std::cin >> temperature;
 
             double result = (temperature - 273.15) * 9 / 5 + 32;
-            std::cout << temperature << "K is " << result << "°F";
+            std::cout << temperature << "K are " << result << "°F";
         }
     }
 
